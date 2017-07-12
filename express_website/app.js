cc76fd40d8d72de1df6b609c4ab6c3ce9a9592f2
Jose Torreblanca <josewhitetower@gmail.com>
2017-07-12 20:04:59 +0200

    Section 3
    res.send(error)

--- express_website/app.js
+++ express_website/app.js
@@ -40,10 +40,12 @@ var mailOptions={
   text: 'You have a submission with the followinf details...Name: '+req.body.name+'Email: '+req.body.email+'Message: '+req.body.message,
   html: '<p>You have a submission with the following details...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
   };
+
+
    transporter.sendMail(mailOptions, function(error, info){
     if(error){
       console.log(error);
-      res.redirect('/');
+      res.send(error);
     } else {
       console.log('Message Sent: '+info.response);
       res.redirect('/');
