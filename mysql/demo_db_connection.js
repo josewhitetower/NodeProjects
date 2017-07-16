34dce7ab92a8c38b126f8b1678d44ef256756b3d
Jose Torreblanca <josewhitetower@gmail.com>
2017-07-16 23:09:43 +0200

    MySQL

---
+++ mysql/demo_db_connection.js
@@ -0,0 +1,33 @@
+var mysql=require('mysql');
+var con = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'root',
+  database:'mydb'
+});
+
+con.connect((err)=>{
+  if (err) throw err;
+  console.log('Connected!');
+ var sql = "INSERT INTO customers (name, address) VALUES ?";
+  var values = [
+    ['John', 'Highway 71'],
+    ['Peter', 'Lowstreet 4'],
+    ['Amy', 'Apple st 652'],
+    ['Hannah', 'Mountain 21'],
+    ['Michael', 'Valley 345'],
+    ['Sandy', 'Ocean blvd 2'],
+    ['Betty', 'Green Grass 1'],
+    ['Richard', 'Sky st 331'],
+    ['Susan', 'One way 98'],
+    ['Vicky', 'Yellow Garden 2'],
+    ['Ben', 'Park Lane 38'],
+    ['William', 'Central st 954'],
+    ['Chuck', 'Main Road 989'],
+    ['Viola', 'Sideway 1633']
+  ];
+  con.query("SELECT * FROM customers",(err,result,fields)=>{
+    if (err) throw err;
+    console.log(fields);
+  });
+});
