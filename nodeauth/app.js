60ff6c07e024e6317b443bd9dd5e1b11eb7ed4c7
Jose Torreblanca <josewhitetower@gmail.com>
2017-07-27 20:20:11 +0200

    Notes

--- nodeauth/app.js
+++ nodeauth/app.js
@@ -4,17 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session=require('express-session');
-var passport= require('passport'); //AUTH Modul
-var expressValidator=require('express-validator');
-var LocalStrategy=require('passport-local').Strategy;
-var multer=require('multer');
-var upload= multer({dest:'./uploads'});
-var flash =require('connect-flash'); //Notifcations messages
-var bcrypt= require('bcryptjs');//To Encrypt passwords
-var mongo=require('mongodb');
-var mongoose=require('mongoose');
-var db =mongoose.connection;
+var session = require('express-session');
+var passport = require('passport'); //AUTH Modul
+var expressValidator = require('express-validator');
+var LocalStrategy = require('passport-local').Strategy;
+var multer = require('multer');
+var upload = multer({ dest: './uploads' });
+var flash = require('connect-flash'); //Notifcations messages
+var bcrypt = require('bcryptjs'); //To Encrypt passwords
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -36,11 +36,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-//handle sessions
+//handle sessions Middleware
 app.use(session({
-  secret:'secret',
-  saveUninitialized:true,
-  resave:true
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
 }));
 
 //passport
@@ -50,32 +50,32 @@ app.use(passport.session());
 
 //validator
 app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.'),
+            root = namespace.shift(),
+            formParam = root;
 
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
     }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
 }));
 
 app.use(require('connect-flash')());
-app.use(function (req, res, next) {
-  res.locals.messages = require('express-messages')(req, res);
-  next();
+app.use(function(req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
 });
 
 //user variable, will be used on templates to show/unshow links(sessions)
-app.get('*', function(req, res, next){
-  res.locals.user=req.user || null;
-  next();
+app.get('*', function(req, res, next) {
+    res.locals.user = req.user || null;
+    next();
 })
 
 app.use('/', index);
@@ -83,20 +83,20 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
